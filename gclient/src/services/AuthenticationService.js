4921a39f12e75799433cb8f627769af2286a9e8d
ajomadlabs <ajojohn@madlabs.xyz>
2017-09-27 23:25:58 +0530

    Fixes #1 - Created a service for Authentication

---
+++ gclient/src/services/AuthenticationService.js
@@ -0,0 +1,12 @@
+// Service for sending and receiving data from the register endpoint
+
+// Importing the Api.js for HTTP
+import Api from '@/services/Api.js'
+
+// Making it exportable
+export default {
+    // Register Method to send user data
+    register (userdata) {
+        return Api().post('register', userdata)
+    }
+}
