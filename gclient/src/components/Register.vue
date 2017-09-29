ee41ddcacce88c2bc47cc63e9fccd51b81902815
ajomadlabs <ajojohn@madlabs.xyz>
2017-09-29 15:37:32 +0530

    Synced with database. Styled with Vuetify

--- gclient/src/components/Register.vue
+++ gclient/src/components/Register.vue
@@ -1,11 +1,22 @@
 <!-- Vue Register View -->
-<template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" placeholder="email" name="email" v-model="email" /><br>
-    <input type="password" placeholder="password" v-model="password"/><br>
-    <button v-on:click="register" type="submit">Register</button>
-  </div>
+<template> 
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+      </div>
+      <div class="pl-0 pr-0 pt-0 pb-2">
+        <v-card>
+          <input type="email" placeholder="email" name="email" v-model="email" /><br>
+          <input type="password" placeholder="password" v-model="password"/><br>
+          <div class="error" v-model="error">{{error}}</div><br>
+          <v-btn class="cyan" v-on:click="register" type="submit">Register</v-btn>
+        </v-card>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 <!-- End -->
 
@@ -18,18 +29,22 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     // Register Method
     // Allows users to register using Email and Password
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -39,4 +54,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .error {
+    color: red;
+  }
 </style>
