b26479b9d5868fad21a7d24b358b2763c4e61c75
ajomadlabs <ajojohn555@gmail.com>
2017-09-30 17:53:09 +0530

    Fixes #1 - Added new styles. Cleaned up the code.

---
+++ gclient/src/components/Header.vue
@@ -0,0 +1,52 @@
+<!-- Component for Header -->
+<template>
+  <v-toolbar fixed class="blue">
+
+    <v-toolbar-title class="mr-4 white--text">
+      <span class="home" v-on:click="navigateTo({name: 'root'})">
+        GuitarTabs
+      </span>
+    </v-toolbar-title>
+
+    <!-- @TODO: Implement Me -->
+    <!--
+    <v-toolbar-items>
+
+      <v-btn flat dark>
+        Browse
+      </v-btn>
+
+    </v-toolbar-items>
+    -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+    
+      <v-btn v-on:click="navigateTo({name: 'register'}) "flat class="white--text">
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
+
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  cursor: pointer;
+}
+.home:hover {
+  color: #000;
+}
+</style>
+<!-- End -->
--- gclient/src/components/Register.vue
+++ gclient/src/components/Register.vue
@@ -1,20 +1,21 @@
-<!-- Vue Register View -->
+<!-- Component for Register-->
 <template> 
   <v-layout>
     <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
+      <div class="white elevation-3">
+        <v-toolbar flat dense class="blue" dark>
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
       </div>
       <!-- Input Styles -->
-      <div class="pl-0 pr-0 pt-0 pb-2">
-        <v-card>
-          <input type="email" placeholder="email" name="email" v-model="email" /><br>
-          <input type="password" placeholder="password" v-model="password"/><br>
-          <div class="error" v-model="error">{{error}}</div><br>
-          <v-btn class="cyan" v-on:click="register" type="submit">Register</v-btn>
-        </v-card>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+        <v-text-field type="email" name="email" label="Email" v-model="email">
+        </v-text-field>
+        <br>
+        <v-text-field type="password" name="password" label="Password" v-model="password"></v-text-field>
+        <br>
+        <div class="error" v-model="error">{{error}}</div><br>
+        <v-btn class="blue" v-on:click="register" type="submit" dark>Register</v-btn>
       </div>
       <!-- End -->
     </v-flex>
