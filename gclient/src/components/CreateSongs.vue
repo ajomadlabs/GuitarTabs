3b3e4ac275d30027aa3adb28f665f7519efc665b
ajomadlabs <ajojohn555@gmail.com>
2017-10-02 00:05:44 +0530

    Fixes #8 - Completed the Create Song Component.

---
+++ gclient/src/components/CreateSongs.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout>
+    <v-flex xs4>
+      <panel title="Add Song">
+        <v-text-field label="Title" type="text" v-model="song.title"></v-text-field>
+        <v-text-field label="Artist" type="text" v-model="song.artists"></v-text-field>
+        <v-text-field label="Genre" type="text" v-model="song.genre"></v-text-field>
+        <v-text-field label="Album" type="text" v-model="song.album"></v-text-field>
+        <v-text-field label="Album Image" type="text" v-model="song.albumImageUrl"></v-text-field>
+        <v-text-field label="Youtube Id" type="text" v-model="song.youtubeId"></v-text-field>
+      </panel>
+    </v-flex>
+    <v-flex xs8>
+      <panel title="Lyrics">
+        <v-text-field label="Lyrics" multi-line v-model="song.lyrics" type="text">Lyrics</v-text-field>
+      </panel>
+      <panel title="Tabs">
+        <v-text-field label="Tab" multi-line v-model="song.tab" type="text">Tabs</v-text-field>
+      </panel>
+      <v-btn class="blue white--text" v-on:click="create">Create Song</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
+export default {
+  data () {
+    return {
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeId: null,
+        lyrics: null,
+        tab: null
+      }
+    }
+  },
+  components: {
+    Panel
+  },
+  methods: {
+    async create () {
+      try {
+        await SongsService.post(this.song)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
--- gclient/src/services/SongsService.js
+++ gclient/src/services/SongsService.js
@@ -3,5 +3,8 @@ import Api from '@/services/Api.js'
 export default {
   index () {
     return Api().get('songs')
+  },
+  post (song) {
+    return Api().post('songs', song)
   }
 }
