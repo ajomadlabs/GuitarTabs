16b4e37522f825894b91b8d4a9218459a7902dc8
ajomadlabs <ajojohn555@gmail.com>
2017-10-02 00:45:17 +0530

    Fixes #9 - Styled the Browse section

--- gclient/src/components/Songs.vue
+++ gclient/src/components/Songs.vue
@@ -5,10 +5,23 @@
         <v-btn v-on:click="navigateTo({name: 'songs-create'})" slot="action" fab class="red white--text" medium absolute right>
           <v-icon>add</v-icon>
         </v-btn>
-        <div v-for="song in songs" :key="songs.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+        <div class="song" v-for="song in songs" :key="songs.id">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-album">
+                {{song.album}}
+              </div>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" v-bind:src="song.albumImageUrl"/>
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -39,4 +52,22 @@ export default {
 </script>
 
 <style scoped>
+ .song {
+   padding: 20px;
+   height: 330px;
+   overflow: hidden;
+ }
+ .song-title {
+  font-size: 30px;
+ }
+ .song-artist {
+  font-size: 24px;
+ }
+ .song-genre {
+  font-size: 18px;
+ }
+ .album-image {
+   width: 100%;
+   margin: 0 auto;
+ }
 </style>
