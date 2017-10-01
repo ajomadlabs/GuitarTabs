38c486d02e6d7fc46081b0e968050b3273b99992
ajomadlabs <ajojohn555@gmail.com>
2017-10-01 19:22:19 +0530

    Fixes #9 - Added a component name Pannel

---
+++ gclient/src/components/Panel.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="blue">
+          <v-toolbar-title class="white--text">{{title}}</v-toolbar-title>
+        </v-toolbar>
+      </div>
+      <div class="pl-4 pr-4 pt-2 pb-2">
+       <slot>
+        No slot Defined.
+       </slot>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: [
+    'title'
+  ]
+}
+</script>
+
+<style scoped>
+</style>
--- gclient/src/components/Songs.vue
+++ gclient/src/components/Songs.vue
@@ -1,17 +1,19 @@
 <template>
   <v-layout>
     <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="blue">
-          <v-toolbar-title class="white--text">Songs</v-toolbar-title>
-        </v-toolbar>
-      </div>
+      <panel title="Songs">
+        
+      </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 export default {
+  components: {
+    Panel
+  }
 }
 </script>
 
