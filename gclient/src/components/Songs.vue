f39c2f9a86fcc144b1a91413679d34cae8d43a98
ajomadlabs <ajojohn555@gmail.com>
2017-10-01 18:58:56 +0530

    Fixes #9 - Added a toolbar. Added a toolbar title names Songs

--- gclient/src/components/Songs.vue
+++ gclient/src/components/Songs.vue
@@ -1,4 +1,13 @@
 <template>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="blue">
+          <v-toolbar-title class="white--text">Songs</v-toolbar-title>
+        </v-toolbar>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
